95a6b0a58717dd43404ba88da38732be4dc5fd57
Akash <sirskyakash@gmail.com>
2026-03-03 01:29:27 +0530

    Backend of url shortner completed

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -5,7 +5,7 @@ import { db } from '../db/index.js';
 import { urlTable } from '../model/url.model.js';
 import authMiddleware, { ensureAuth } from '../middleware/auth.middleware.js';
 import { createUrl } from '../services/url.service.js';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
 
 
@@ -29,6 +29,26 @@ router.post('/shorten', authMiddleware, ensureAuth, async function (req, res) {
     }
 })
 
+router.get('/codes', ensureAuth, async function (req, res) {
+    const result = await db.select({
+        shortCode: urlTable.shortCode,
+        targetUrl: urlTable.targetUrl,
+        id: urlTable.id,
+        createdAt: urlTable.createdAt,
+        updatedAt: urlTable.updatedAt
+    }).from(urlTable).where(eq(urlTable.userId, req.user.id));
+    return res.status(200).json({ data: { result } });
+})
+
+router.delete('/:id', ensureAuth, async function (req, res) {
+    const id = req.params.id;
+    const [result] = await db.delete(urlTable).where(and(eq(urlTable.id, id), eq(urlTable.userId, req.user.id))).returning({
+        shortCode: urlTable.shortCode,
+        targetUrl: urlTable.targetUrl
+    });
+    return res.status(200).json({ data: { result } });
+})
+
 router.get('/:shortCode', async function (req, res) {
     const code = req.params.shortCode;
     const [result] = await db.select({
@@ -42,3 +62,4 @@ router.get('/:shortCode', async function (req, res) {
 })
 
 export default router;
+
